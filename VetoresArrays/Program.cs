23dd3c49a42eb97bcc8c139fd33721da893db7ab
João Junior <batistasousajoao@gmail.com>
2023-11-07 10:11:59 -0300

    Adicionar arquivos de projeto.

---
+++ VetoresArrays/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace Curso
+{
+    public class Arrays1
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double[] vect = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            double soma = 0.0;
+            for (int i = 0; i < n; ++i)
+            {
+                soma += vect[i];
+            }
+
+            double avg = soma / n;
+
+            Console.WriteLine("AVERAGE HEIGHT: " + avg.ToString("F2", CultureInfo.InvariantCulture));
+
+
+
+        }
+    }
+}
